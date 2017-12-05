49f1a1b103ffee65daa4ecb1c84bba8361e02b56
Albert Zhu <zalbert9@gmail.com>
2017-12-05 00:41:47 -0500

    main menu is functional (except for networking)

---
+++ Assets/Scripts/ActivatePauseMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatePauseMenu : MonoBehaviour {
+
+	public GameObject menu; // Assign in inspector
+	private bool isShowing;
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.P)) {
+			Debug.Log ("Pause Menu");
+			isShowing = !isShowing;
+			menu.SetActive(isShowing);
+		}
+	}
+
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public void PlayGame() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void OpenOptionMenu() {
+
+	}
+
+	public void QuitGame() {
+		Application.Quit();
 	}
 }
