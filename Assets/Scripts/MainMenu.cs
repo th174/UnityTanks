9ce7c53617fc856c6bce9d167d48a2775d35ac0d
Albert Zhu <zalbert9@gmail.com>
2017-12-05 13:15:03 -0500

    input field for server number

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,25 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+	public InputField serverNumber;
+
+	void Update() {
+		Debug.Log (serverNumber.text);
+	}
+
     public void CreateGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         SceneParameters.isHost = true;
     }
 
-    public void JoinGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        SceneParameters.isHost = false;
-    }
-
-    public void OpenOptionMenu()
-    {
-
-    }
+//    public void JoinGame()
+//    {
+//        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+//        SceneParameters.isHost = false;
+//    }
 
     public void QuitGame()
     {
